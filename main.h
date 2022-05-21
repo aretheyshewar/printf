92a8320a963e1bc63708eb0ed61649e3f2e4f2f9
follyblink <follyblink@gmail.com>
2022-05-21 08:39:37 -0700

    main.h

--- main.h
+++ main.h
@@ -1,4 +1,3 @@
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -8,21 +7,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-/*int _putchar(char c);*/
+int _putchar(char c);
+int _strlen(char *str);
 int _printf(const char *format, ...);
 char *print_char(va_list ap);
 char *print_str(va_list ap);
+char *print_int(va_list ap);
+char *convert_int_to_str(int multiple, int length, int n);
 char *_strcpy(char *dest, char *src);
 char* (*select_func(char c))(va_list);
 char *string_buffer(void);
-void realloc_buffer(char *buffer, int len);
+void realloc_buffer(char *buffer, int len, va_list ap);
 int buffer_overflow(char *buffer, int pos);
-
-
-typedef struct function
-{
-	char op;
-	char* (*func)(va_list);
-} functn_t;
 
 #endif
